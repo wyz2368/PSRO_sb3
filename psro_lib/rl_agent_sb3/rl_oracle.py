0de55b60aa3742f5f3670ed11707ab36e4976ce8
wangyzh <wangyzh@umich.edu>
2024-09-17 22:17:09 +0100

    "add policy arch"

--- psro_lib/rl_agent_sb3/rl_oracle.py
+++ psro_lib/rl_agent_sb3/rl_oracle.py
@@ -81,9 +81,12 @@ class RLOracle(optimization_oracle.AbstractOracle):
       if not copy_from_prev:
         # Create a DRL policy.
         # TODO: SAC may need action noise. Possibly callbacks.
+        policy_arch = [self._best_response_kwargs["hidden_layers_sizes"]] * self._best_response_kwargs["hidden_layers"]
+        policy_kwargs = dict(net_arch=[dict(pi=policy_arch, vf=policy_arch)])
         nn = generate_agent_policy(self._best_response_kwargs["policy"])
         policy = self._best_response_class(policy=nn,
                                            env=self.gym_env,
+                                           policy_kwargs=policy_kwargs,
                                            verbose=self.verbose)
       # Copy from previous iterations.
       else:
