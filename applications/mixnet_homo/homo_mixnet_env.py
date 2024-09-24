9453968cd1c5ba8f4d512874605a70dc03fe951b
wangyzh <wangyzh@umich.edu>
2024-09-24 17:34:36 +0100

    "vary params"

--- applications/mixnet_homo/homo_mixnet_env.py
+++ applications/mixnet_homo/homo_mixnet_env.py
@@ -49,10 +49,10 @@ class Mixnet_env(AECEnv):
                             a_attack_cost=[-400, -500, -400], # attacker's attacking cost
                             a_deploy_cost=[-200, -200, -200], # attacker's deployment cost
                             a_maintain_cost=[-50, -50, -50], # attacker's maintaining cost
-                            active_rate=[0.6, 0.6, 0.6], # the possibility of sucessfully activate a node
+                            active_rate=[0.6, 0.3, 0.3], # the possibility of sucessfully activate a node
                             d_exclude_cost=[-300, -300, -300], # defender's cost on excluding a node
                             d_deploy_cost=[-200, -200, -200], # defender's deployment cost
-                            d_maintain_cost=[-30, -30, -30],
+                            d_maintain_cost=[-50, -150, -150],
                             usage_threshold=0.03, # the lower bound of usage without penalty
                             d_penalty=-50, # defender's penalty for insufficient usage
                             a_alpha=60000,  # coefficient for the reward
